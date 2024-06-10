cfd8e8b96b38d089368842557d12a7167ae529a0
Tollef Jørgensen <tollefj@gmail.com>
2024-06-10 23:07:32 +0200

    env changes

--- serve.sh
+++ serve.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source .env
 echo "$LLM_TOKENIZER"
-python3 -m llama_cpp.server --model "$LLM_PATH" --n_gpu_layers=-1 --chat_format chatml --n_ctx=8168
+python3 -m llama_cpp.server --model "$LLM_PATH" --n_gpu_layers=-1 --chat_format chatml --n_ctx=4096
